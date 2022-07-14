60f48b055ec047938a5a644226ffd3eec8e19611
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2022-07-14 17:56:25 +0300

    initial thought
    Still testing ...

---
+++ types.ts
@@ -0,0 +1,29 @@
+export interface IHARF {
+  name: string
+  weight: number
+  value: string
+  description?: string
+  color?: string
+  encoding?: string
+}
+export interface IkALEMAT {
+  horuf: IHARF[]
+}
+export interface IAYA {
+  Kalemat: IkALEMAT[]
+}
+export interface ISURA {
+  Ayat: IAYA[]
+}
+export interface IQuran {
+  Surah: ISURA[]
+}
+export interface ISTATE {
+  quran: IQuran
+  Surah: ISURA[]
+  Index: IQuran
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface IQ2B {
+  state: ISTATE
+}
